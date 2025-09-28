0f1c320127248bae5680033cfadda5cea3a7bd83
chann44 <44chansw@gmail.com>
2025-09-28 05:57:05 +0530

    frontend fixes

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -5,6 +5,7 @@ import { Nav } from "@/components/nav";
 import { ThemeProvider } from "@/components/theme-provider";
 import { LoadingScreen } from "@/components/loading-screen";
 import { useState, useEffect } from "react";
+import { FloatingPaths } from "@/components/ui/floating-paths";
 
 function RootComponent() {
   const [isLoading, setIsLoading] = useState(true);
@@ -57,8 +58,9 @@ function RootComponent() {
   }
 
   return (
+    <div className="min-h-screen relative">
     <ThemeProvider defaultTheme="dark">
-      <div className="max-w-7xl mx-auto dark">
+      <div className="max-w-7xl mx-auto ">
         <ConnectWalletProvider>
           <Nav />
           <div className="p-4 md:p-10">
@@ -66,8 +68,11 @@ function RootComponent() {
             <TanStackRouterDevtools />
           </div>
         </ConnectWalletProvider>
+        <FloatingPaths position={1}  />
       </div>
     </ThemeProvider>
+    </div>
+
   );
 }
 
